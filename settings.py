fcbb678c71a7fac83219dcec4f6bc33da5b5fa59
vbanos <vbanos@gmail.com>
2014-11-10 21:36:34 +0200

    updated settings

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 #
 # parameters used in all scripts
 #
-mysql_connection = unicode("mysql+pymysql://root:blogforever!23#@localhost:3306/cms_survey")
+mysql_connection = unicode("mysql+pymysql://username:password@localhost:3306/cms_survey")
 
